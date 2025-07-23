f04c6a5a3b265b58c98b3e88c8434bef54222504
Ytsixx <evaristobilly52@gmail.com>
2025-07-23 16:06:53 +0200

    mensagem anima

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,26 @@
+import Head from 'next/head'
+
 export default function SixxHxRx() {
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-      margin: 0
-    }}>
-      <h1>SixxHxRx</h1>
-    </div>
+    <>
+      <Head>
+        <title>Sixx Cloud – Teu Site Profissional</title>
+        <meta name="description" content="A plataforma SixxCloud para hospedagem e soluções tecnológicas." />
+        <meta property="og:title" content="Sixx Cloud – Teu Site Profissional" />
+        <meta property="og:description" content="A plataforma SixxCloud para hospedagem e soluções tecnológicas." />
+        <meta property="og:image" content="https://files.catbox.moe/bo4gra.jpeg" />
+        <meta property="og:url" content="https://sixxcloud.vercel.app" />
+      </Head>
+
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        margin: 0
+      }}>
+        <h1>SixxHxRx</h1>
+      </div>
+    </>
   );
 }
